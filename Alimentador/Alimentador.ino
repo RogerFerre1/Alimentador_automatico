2db69081c3727c816c5a9a7b6b037a411bdd8d12
Rogin <rogerferresouza2@gmail.com>
2026-09-05 22:18:14 -0300

    Implementada função de medição de nível do reservatório

--- Alimentador/Alimentador.ino
+++ Alimentador/Alimentador.ino
@@ -5,17 +5,20 @@
 // Pinos/Variáveis
 
 Servo servo1;
-Servo servo2;
+Servo servo2; 
 
 const int servo1_PIN = 13;
 const int servo2_PIN = 12;
 const int sensorIR = 35;
+const int sensorLaser = 14;
 
 int anguloFechado = 0;
 int anguloAberto = 180;
 
 void setup(){
   pinMode(sensorIR, INPUT);
+  pinMode(sensorLaser, INPUT);
+  Serial.begin(9600);
   
   servo1.attach(servo1_PIN);
   servo2.attach(servo2_PIN);
@@ -35,6 +38,16 @@ void loop(){
   }
 
   delay(500);
+
+  int estadoLDR = digitalRead(sensorLaser);
+  
+  if(estadoLDR == LOW){
+    Serial.println("Claro");
+  } else {
+    Serial.println("Escuro");
+  }
+
+  delay(1000);
 }
 
 void moverServos(int angulo){
